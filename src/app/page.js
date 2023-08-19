0f33625613822ff246986370018216c67166e5fb
jelenjuice <jules.risos@gmail.com>
2023-08-19 16:47:41 +1000

    --all

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import image from "next/image";
+import Header from "../../components/Header.jsx";
+
+export default function Home() {
+    return (
+        <html>
+            <body>
+              <Header name="John"/>
+            <h1>Hello</h1>
+            </body>
+        </html>
+    )
+}
